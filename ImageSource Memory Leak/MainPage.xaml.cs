e489b7ae17f192b6c685c78a592fee446ba22d25
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-07-16 19:34:08 -0300

    Refactor UI and enhance memory management
    - Updated AppShell.xaml to load `FirstPage` as the initial page, with minor formatting adjustments.
    - Disabled nullable reference types in the project file to change compiler nullability checks.
    - Added `OnNavigatedFrom` in MainPage.xaml.cs to force garbage collection when navigating away, and introduced a new `FirstPage` class with a method to explicitly call garbage collection.
    - MainPageViewModel.cs now uses a `WeakReference<Timer>` for better memory management, added a new `Timer` property for encapsulated access, and updated the destructor with a comment on finalizer execution.
    - Minor unchanged functionality in `ImageGenerator` class, indicating a focus on memory management and performance improvements.

--- ImageSource Memory Leak/MainPage.xaml.cs
+++ ImageSource Memory Leak/MainPage.xaml.cs
@@ -5,11 +5,50 @@ using System.Threading;
 
 namespace ImageSource_Memory_Leak
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
+		{
+			FirstPage.FullGC();
+			FirstPage.FullGC();
+		}
+	}
+
+	sealed class FirstPage : ContentPage
+	{
+		public FirstPage()
+		{
+			var btn = new Button
+			{
+				Text = "Navigate",
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			btn.Clicked += async (_, __) =>
+			{
+				FullGC();
+				await Shell.Current.Navigation.PushAsync(new MainPage());
+			};
+
+			Content = btn;
+		}
+
+
+
+
+		public static void FullGC()
+		{
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+		}
+	}
 }
+
+
